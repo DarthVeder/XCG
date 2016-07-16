0b4e6d51a4ec91e9444ad33ad141afcf5d312b1a
DarthVeder <mm191274@gmail.com>
2016-07-16 17:40:21 +0200

    Adding user menu

---
+++ menu.h
@@ -0,0 +1,21 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+
+using namespace std;
+
+enum choice
+{
+    QUIT,
+    READFILE,
+    SAVEFILE,
+    PRINTISA
+};
+
+choice menu();
+
+
+#endif /* MENU_H */
+
+
